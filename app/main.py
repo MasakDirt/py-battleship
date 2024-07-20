ca6a7b58072d1601e65bbd25016fc642707e2b16
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-20 18:19:34 +0300

    added some improvements

--- app/main.py
+++ app/main.py
@@ -1,40 +1,49 @@
 from collections import Counter
 
 
+class NumberOfShipsError(Exception):
+    pass
+
+
 class Deck:
     def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
-        self.row = row
-        self.column = column
-        self.is_alive = is_alive
+        self.__row = row
+        self.__column = column
+        self.__is_alive = is_alive
 
     def __repr__(self) -> str:
         return (f"Deck(row={self.row}, "
                 f"column={self.column}, live={self.is_alive})")
 
+    @property
+    def is_alive(self) -> bool:
+        return self.__is_alive
+
+    @is_alive.setter
+    def is_alive(self, is_alive: bool) -> None:
+        self.__is_alive = is_alive
+
+    @property
+    def row(self) -> int:
+        return self.__row
+
+    @property
+    def column(self) -> int:
+        return self.__column
+
 
 class Ship:
-    def __init__(self, start: tuple, end: tuple) -> None:
-        self.decks = []
-        self.__fill_decks(start, end)
-
-    def __fill_decks(self, start: tuple, end: tuple) -> None:
-        if start == end:
-            self.decks.append(Deck(start[0], start[1]))
-        elif start[0] == end[0]:
-            for i in range(start[1], end[1] + 1):
-                self.decks.append(Deck(start[0], i))
-        else:
-            for i in range(start[0], end[0] + 1):
-                self.decks.append(Deck(i, start[1]))
+    def __init__(self, coordinates: tuple) -> None:
+        self.__decks = [Deck(coord[0], coord[1]) for coord in coordinates]
 
     def get_deck(self, row: int, column: int) -> Deck | None:
-        for deck in self.decks:
+        for deck in self.__decks:
             if deck.row == row and deck.column == column:
                 return deck
         return None
 
     def is_last_alive(self, row: int, column: int) -> bool:
-        sum_of_alive = sum(deck.is_alive for deck in self.decks)
+        sum_of_alive = sum(deck.is_alive for deck in self.__decks)
         if sum_of_alive > 1:
             return False
         current_deck = self.get_deck(row, column)
@@ -53,12 +62,12 @@ class Ship:
         return "Miss!"
 
     def __repr__(self) -> str:
-        return str(self.decks)
+        return str(self.__decks)
 
 
 class Battleship:
     def __init__(self, ships: list[tuple]) -> None:
-        self.field = {}
+        self.__field = {}
         for ship in ships:
             start = ship[0]
             end = ship[1]
@@ -77,28 +86,32 @@ class Battleship:
             for i in range(start[0], end[0] + 1):
                 result_list.append((i, start[1]))
 
-        self.field[tuple(result_list)] = Ship(start, end)
+        result_tuple = tuple(result_list)
+        self.__field[result_tuple] = Ship(result_tuple)
 
     def fire(self, location: tuple) -> str:
-        for cords in self.field:
+        for cords in self.__field:
             if location in cords:
-                return self.field[cords].fire(location[0], location[1])
+                return self.__field[cords].fire(location[0], location[1])
 
         return "Miss!"
 
-    def __repr__(self) -> str:
-        return str(self.field)
-
     def _validate_field(self) -> None:
-        assert len(self.field) == 10, \
-            "the total number of the ships should be 10"
+        if len(self.__field) != 10:
+            raise NumberOfShipsError(
+                f"The total number of the ships should "
+                f"be 10, but you have {len(self.__field)}")
 
         expected_dict = {"1": 4, "2": 3, "3": 2, "4": 1}
-        counter = Counter(str(len(cords)) for cords in self.field)
-        assert expected_dict == counter, \
-            """In Battleship should be:
-                 4 single-deck ships;
-                 3 double-deck ships;
-                 2 three-deck ships;
-                 1 four-deck ship;
-            """
+        counter = Counter(str(len(cords)) for cords in self.__field)
+        if expected_dict != counter:
+            raise NumberOfShipsError(
+                f"In Battleship should be: "
+                f"\n4 single-deck ships, but you have {counter["1"]}"
+                f"\n3 double-deck ships, but you have {counter["2"]}"
+                f"\n2 three-deck ships, but you have {counter["3"]}"
+                f"\n1 four-deck ship, but you have {counter["4"]}"
+            )
+
+    def __repr__(self) -> str:
+        return str(self.__field)
